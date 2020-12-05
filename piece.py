3537e5713457ae94ce94658fb137c136be1cfe53
snehawattamwar <snehaw993@gmail.com>
2020-12-05 17:25:49 -0500

    Add files via upload

---
+++ piece.py
@@ -0,0 +1,20 @@
+COLOR_LIGHT = 'light'
+COLOR_DARK = 'dark'
+
+class Piece:
+    def __init__(self, color):
+        self.color = color
+        # self.king = False
+
+    # def become_king(self):
+    #     """Sets king flag to true."""
+    #
+    #     self.king = True
+
+class LightPiece(Piece):
+    def __init__(self, color=COLOR_LIGHT):
+        super().__init__(color)
+
+class DarkPiece(Piece):
+    def __init__(self, color=COLOR_DARK):
+        super().__init__(color)
